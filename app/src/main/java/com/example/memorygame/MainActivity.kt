8470c0dfe46785c037a088642a2cfd7830bfb41b
Fabiano <fabiano.filho@aluno.unc.br>
2021-10-18 17:46:00 -0300

    Radio button diferenciado activity_main.xml

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.memorygame
 
+
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RadioGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import com.example.memorygame.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var binding: ActivityMainBinding
     private lateinit var btnPlay: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         btnPlay = findViewById(R.id.btnPlay)
+
 
         btnPlay.setOnClickListener {
             val intent = Intent(this, Game::class.java)
             startActivity(intent);
         }
+
     }
 
 
 
-
 }
+
+
+
+
